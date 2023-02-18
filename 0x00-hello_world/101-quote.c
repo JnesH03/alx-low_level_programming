55a5d5912563daacfc4f633535e00fc78f9376d0
Josphat Munene <munenejosphat17@gmail.com>
2023-02-18 08:01:06 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+* Return: 0
+*/
+int main(void)
+{
+        printf("and that piece of art is useful" - Dora Korpar, 2015-10-19,/n");
+                return (0);
+}
+
