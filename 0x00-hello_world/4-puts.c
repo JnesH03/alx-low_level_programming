657c06fa17962a23339a2e51432385ff719a0d17
Josphat Munene <munenejosphat17@gmail.com>
2023-02-18 04:17:03 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,6 @@
+
   #include <stdio.h>
+
   /**
   * main - print the string in the put function
   *
@@ -7,7 +9,7 @@
   * Return: 0
   */
   int main(void)
-  { 
- 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+  {  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
   }
