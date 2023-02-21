31d291474465daeb69bd070ddc73e45113709080
Josphat Munene <munenejosphat17@gmail.com>
2023-02-21 09:20:53 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
-#include "main.h"
-/* print alphabets- function to print abc
+#include "holberton.h"
+/**
+ * print_alphabet - function to print abc
  *
  * Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
-	_putchar('\n');
-}
+_putchar('\n');
+}}
